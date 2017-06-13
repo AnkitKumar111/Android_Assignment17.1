47f89a245717f35291c93630c3972cfddb585a04
AnkitKumar111 <ankitwelcomesyou@gmail.com>
2017-06-12 22:38:05 -0700

    Android_Assignment17.1
    A music player that plays music on start button

---
+++ app/src/main/java/com/example/ankitkumar/music_player/MusicService.java
@@ -0,0 +1,43 @@
+package com.example.ankitkumar.music_player;
+
+/**
+ * Created by Ankit Kumar on 6/11/2017.
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MusicService extends Service {
+
+    MediaPlayer musicPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mission);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        musicPlayer.start();
+        musicPlayer.setLooping(true);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        musicPlayer.stop();
+        musicPlayer.release();
+        super.onDestroy();
+    }
+}
